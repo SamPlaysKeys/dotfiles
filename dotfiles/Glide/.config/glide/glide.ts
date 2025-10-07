601bd8884a7ab0418725d2b4d71e9dd3c11b13e1
Sam Fleming <91982544+SamPlaysKeys@users.noreply.github.com>
2025-10-07 00:50:12 -0400

    Add Glide and GoNeovim configurations

---
+++ dotfiles/Glide/.config/glide/glide.ts
@@ -0,0 +1,22 @@
+// Config docs:
+//
+//   https://glide-browser.app/config
+//
+// API reference:
+//
+//   https://glide-browser.app/api
+//
+// Default config files can be found here:
+//
+//   https://github.com/glide-browser/glide/tree/main/src/glide/browser/base/content/plugins
+//
+// Most default keymappings are defined here:
+//
+//   https://github.com/glide-browser/glide/blob/main/src/glide/browser/base/content/plugins/keymaps.mts
+//
+// Try typing `glide.` and see what you can do!
+
+glide.o.hint_size = "30px";
+
+glide.keymaps.set(["normal", "insert"], "<Leader>h", "back");
+glide.keymaps.set(["normal", "insert"], "<Leader>l", "forward");
