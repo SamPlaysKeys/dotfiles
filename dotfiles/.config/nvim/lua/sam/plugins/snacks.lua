960ca4622e1ad4dc73f2f45edbe63b0fb774146d
SamPlaysKeys <sam@samplayskeys.com>
2026-02-24 16:02:59 -0500

    Updating the Dashboard from alpha to Snacks

--- dotfiles/.config/nvim/lua/sam/plugins/snacks.lua
+++ dotfiles/.config/nvim/lua/sam/plugins/snacks.lua
@@ -2,6 +2,15 @@ return {
   "folke/snacks.nvim",
   priority = 1000,
   lazy = false,
+  -- init = function()
+  --   vim. api.nvim_create_autocmd( "ColorScheme", {
+  --   pattern ="*",
+  --   callback = function()
+  --   vim.api.nvim_set_hl(0, "SnacksDashboardHeader", { fg = "#cc0000" })
+  --   end,
+  --   })
+  --   vim.api.nvim_set_hl(0, "SnacksDashboardHeader", { fg = "#cc0000" })
+  --   end,
   opts = {
     -- Enables the better `vim.ui.input`
     input = { enabled = true },
@@ -19,6 +28,34 @@ return {
     scope = { enabled = true },
     -- Toggle system for various settings
     toggle = { enabled = true },
+    -- Dashboard configuration replacing alpha.nvim
+    dashboard = {
+      enabled = true,
+      preset = {
+        header = [[
+ ██████████               ███████████ █████                  ███████████ █████       ███                     
+▒▒███▒▒▒▒███             ▒█▒▒▒███▒▒▒█▒▒███                  ▒█▒▒▒███▒▒▒█▒▒███       ▒▒▒                      
+ ▒███   ▒▒███  ██████    ▒   ▒███  ▒  ▒███████    ██████    ▒   ▒███  ▒  ▒███████   ████  ████████    ███████
+ ▒███    ▒███ ███▒▒███       ▒███     ▒███▒▒███  ███▒▒███       ▒███     ▒███▒▒███ ▒▒███ ▒▒███▒▒███  ███▒▒███
+ ▒███    ▒███▒███ ▒███       ▒███     ▒███ ▒███ ▒███████        ▒███     ▒███ ▒███  ▒███  ▒███ ▒███ ▒███ ▒███
+ ▒███    ███ ▒███ ▒███       ▒███     ▒███ ▒███ ▒███▒▒▒         ▒███     ▒███ ▒███  ▒███  ▒███ ▒███ ▒███ ▒███
+ ██████████  ▒▒██████        █████    ████ █████▒▒██████        █████    ████ █████ █████ ████ █████▒▒███████
+▒▒▒▒▒▒▒▒▒▒    ▒▒▒▒▒▒        ▒▒▒▒▒    ▒▒▒▒ ▒▒▒▒▒  ▒▒▒▒▒▒        ▒▒▒▒▒    ▒▒▒▒ ▒▒▒▒▒ ▒▒▒▒▒ ▒▒▒▒ ▒▒▒▒▒  ▒▒▒▒▒███
+                                                                                                     ███ ▒███
+                                                                                                    ▒▒██████ 
+                                                                                                     ▒▒▒▒▒▒  
+        ]],
+        keys = {
+          { icon = " ", key = "e", desc = "New File", action = ":ene | startinsert" },
+          { icon = " ", key = "t", desc = "Toggle file explorer", action = ":NvimTreeToggle" },
+          { icon = "󰱼 ", key = "f", desc = "Find File", action = ":Telescope find_files" },
+          { icon = " ", key = "s", desc = "Find Word", action = ":Telescope live_grep" },
+          { icon = "󰁯 ", key = "r", desc = "Restore Session", action = ":SessionRestore" },
+          { icon = "󰒲 ", key = "l", desc = "Lazy Plugins", action = ":Lazy" },
+          { icon = " ", key = "q", desc = "Quit NVIM", action = ":qa" },
+        },
+      },
+    },
   },
   keys = {
     { "<leader>uw", function() Snacks.toggle.option("list"):toggle() end, desc = "Toggle Whitespace (listchars)" },
