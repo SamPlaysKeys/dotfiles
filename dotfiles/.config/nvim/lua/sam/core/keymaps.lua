d19aaaa4c582b18965eac8198e92cf89ffc289d5
Sam Fleming <91982544+SamPlaysKeys@users.noreply.github.com>
2026-03-26 20:11:43 -0400

    Added more terminal hotkeys

--- dotfiles/.config/nvim/lua/sam/core/keymaps.lua
+++ dotfiles/.config/nvim/lua/sam/core/keymaps.lua
@@ -28,11 +28,12 @@ keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- 
 keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
 keymap.set("n", "<leader>st", "<cmd>vsplit | term<CR><cmd>startinsert<CR>", { desc = "Split into Terminal" }) -- Split current window, opening terminal
 
-keymap.set("n", "<leader>to", "<cmd>TabNewEntered<CR>", { desc = "Open new tab" }) -- open new tab
+keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
 keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
 keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
 keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
+keymap.set("n", "<leader>tt", "<cmd>tabnew<CR><cmd>terminal<CR><cmd>startinsert<CR>", { desc = "Open Terminal in a new tab" }) -- open new tab, launch terminal, switch to insert
 
 -- Exit the "Terminal Mode" with Ctrl + Space
 keymap.set("t", '<C-Space>', "<C-\\><C-n>",{silent = true})
