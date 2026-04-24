88f93b39440c0da095cb4dc2e2fe14c3d8ecc4aa
SamPlaysKeys <sam@samplayskeys.com>
2026-04-24 12:23:56 -0400

    Removed old docs

--- dotfiles/config/nvim/IDEAS.md
+++
@@ -1,1 +0,0 @@
-Idea for persistance: Create a custom lua plugin to allow locking a tab so that it can't be closed. Ideally, it should update the tab view to indicate that it is locked.
--- dotfiles/config/nvim/lua/plugins/codediff.lua
+++ dotfiles/config/nvim/lua/plugins/codediff.lua
@@ -8,7 +8,7 @@ return {
     require("codediff").setup({
         -- You can add custom configuration here if needed
     })
-    
+
     -- Keymaps for codediff
     vim.keymap.set("v", "<leader>cd", "<cmd>CodeDiff<cr>", { desc = "Compare selected code blocks" })
   end,
