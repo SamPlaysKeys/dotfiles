6b70e8ab321676a1b781ad1dae1bc991b17ebae0
SamPlaysKeys <sam@samplayskeys.com>
2026-04-24 12:03:50 -0400

    Updated theme colors, and set powerline separators

--- dotfiles/config/nvim/lua/plugins/lualine.lua
+++ dotfiles/config/nvim/lua/plugins/lualine.lua
@@ -16,30 +16,33 @@ return {
       inactive_bg = "#2c3043",
     }
 
+    local branch_bg = "#45475a"
+    local branch_fg = "#cdd6f4"
+
     local my_lualine_theme = {
       normal = {
         a = { bg = colors.blue, fg = colors.bg, gui = "bold" },
-        b = { bg = colors.bg, fg = colors.fg },
+        b = { bg = branch_bg, fg = branch_fg },
         c = { bg = colors.bg, fg = colors.fg },
       },
       insert = {
         a = { bg = colors.green, fg = colors.bg, gui = "bold" },
-        b = { bg = colors.bg, fg = colors.fg },
+        b = { bg = branch_bg, fg = branch_fg },
         c = { bg = colors.bg, fg = colors.fg },
       },
       visual = {
         a = { bg = colors.violet, fg = colors.bg, gui = "bold" },
-        b = { bg = colors.bg, fg = colors.fg },
+        b = { bg = branch_bg, fg = branch_fg },
         c = { bg = colors.bg, fg = colors.fg },
       },
       command = {
         a = { bg = colors.yellow, fg = colors.bg, gui = "bold" },
-        b = { bg = colors.bg, fg = colors.fg },
+        b = { bg = branch_bg, fg = branch_fg },
         c = { bg = colors.bg, fg = colors.fg },
       },
       replace = {
         a = { bg = colors.red, fg = colors.bg, gui = "bold" },
-        b = { bg = colors.bg, fg = colors.fg },
+        b = { bg = branch_bg, fg = branch_fg },
         c = { bg = colors.bg, fg = colors.fg },
       },
       inactive = {
@@ -53,10 +56,12 @@ return {
     lualine.setup({
       options = {
         theme = my_lualine_theme,
+        section_separators = { left = "\u{e0b0}", right = "\u{e0b2}" },
+        component_separators = { left = "\u{e0b1}", right = "\u{e0b3}" },
       },
       sections = {
         lualine_b = {
-          { "branch", color = { bg = "#45475a", fg = "#cdd6f4" } },
+          { "branch" },
         },
         lualine_c = {
           { "filename", path = 1 },  -- 0=filename, 1=relative path, 2=absolute, 3=absolute with ~
