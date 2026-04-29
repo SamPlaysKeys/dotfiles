939ed11a6f71fcb1793d3a1dfcf5533c8a53a5c8
SamPlaysKeys <sam@samplayskeys.com>
2026-04-29 16:50:57 -0400

    Add dropbar to ui.lua

--- configs/nvim/lua/plugins/ui.lua
+++ configs/nvim/lua/plugins/ui.lua
@@ -25,6 +25,26 @@ return {
   },
 
 
+
+  -----------------------------------------------------------------------------
+  -- NOTE: dropbar: VSCode-like file path and navigation
+  -----------------------------------------------------------------------------
+{
+  'Bekaboo/dropbar.nvim',
+  -- optional, but required for fuzzy finder support
+  dependencies = {
+    'nvim-telescope/telescope-fzf-native.nvim',
+    build = 'make'
+  },
+  opts = {},
+  config = function()
+    local dropbar_api = require('dropbar.api')
+    vim.keymap.set('n', '<Leader>;', dropbar_api.pick, { desc = 'Pick symbols in winbar' })
+    vim.keymap.set('n', '[;', dropbar_api.goto_context_start, { desc = 'Go to start of current context' })
+    vim.keymap.set('n', '];', dropbar_api.select_next_context, { desc = 'Select next context' })
+  end
+},
+
   -----------------------------------------------------------------------------
   -- NOTE: bufferline.nvim: Tab Bar
   -----------------------------------------------------------------------------
