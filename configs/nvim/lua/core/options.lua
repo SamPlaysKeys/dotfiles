1a844e64cc99c2015c793e5cc43e7bc6cb29c02e
SamPlaysKeys <sam@samplayskeys.com>
2026-04-29 17:14:13 -0400

    Keep dividers colored but thin

--- configs/nvim/lua/core/options.lua
+++ configs/nvim/lua/core/options.lua
@@ -40,15 +40,15 @@ opt.splitright = true -- split vertical window to the right
 opt.splitbelow = true -- split horizontal window to the bottom
 
 -- Make window separators bolder
-opt.fillchars = {
-  vert = "┃",
-  horiz = "━",
-  horizup = "┻",
-  horizdown = "┳",
-  vertleft = "┫",
-  vertright = "┣",
-  verthoriz = "╋",
-}
+-- opt.fillchars = {
+--   vert = "┃",
+--   horiz = "━",
+--   horizup = "┻",
+--   horizdown = "┳",
+--   vertleft = "┫",
+--   vertright = "┣",
+--   verthoriz = "╋",
+-- }
 
 -- whitespace visualization
 opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
