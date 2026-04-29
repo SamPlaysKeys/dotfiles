9cd5045f3cd7f9592fd7c7d548a3ef507431bc2f
SamPlaysKeys <sam@samplayskeys.com>
2026-04-29 15:31:32 -0400

    Consolidate themed items into a single file

---
+++ configs/nvim/lua/plugins/theme.lua
@@ -0,0 +1,78 @@
+-- Theme Collection
+--
+-- Consolidates colorscheme and transparency plugins.
+
+return {
+  -----------------------------------------------------------------------------
+  -- Colorscheme: Catppuccin
+  -- A warm, pastel colorscheme. Using "mocha" (dark) flavor.
+  -- Integrates with treesitter, cmp, gitsigns, nvim-tree, and mini.
+  -----------------------------------------------------------------------------
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+      require("catppuccin").setup({
+        flavour = "mocha", -- latte, frappe, macchiato, mocha
+        background = { -- :h background
+          light = "latte",
+          dark = "mocha",
+        },
+        transparent_background = true, -- disables setting the background color.
+        show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
+        term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
+        dim_inactive = {
+          enabled = false, -- dims the background color of inactive window
+          shade = "dark",
+          percentage = 0.15, -- percentage of the shade to apply to the inactive window
+        },
+        no_italic = false, -- Force no italic
+        no_bold = false, -- Force no bold
+        no_underline = false, -- Force no underline
+        styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
+          comments = { "italic" }, -- Change the style of comments
+          conditionals = { "italic" },
+          loops = {},
+          functions = {},
+          keywords = {},
+          strings = {},
+          variables = {},
+          numbers = {},
+          booleans = {},
+          properties = {},
+          types = {},
+          operators = {},
+        },
+        color_overrides = {},
+        custom_highlights = {},
+        integrations = {
+          cmp = true,
+          gitsigns = true,
+          nvimtree = true,
+          treesitter = true,
+          notify = false,
+          mini = {
+            enabled = true,
+            indentscope_color = "",
+          },
+        },
+      })
+
+      -- setup must be called before loading
+      vim.cmd.colorscheme "catppuccin"
+    end,
+  },
+
+  -----------------------------------------------------------------------------
+  -- Transparent background plugin
+  -- https://github.com/xiyaowong/transparent.nvim
+  -----------------------------------------------------------------------------
+  {
+    "xiyaowong/transparent.nvim",
+    lazy = false, -- Load immediately to prevent flicker on startup
+    keys = {
+      { "<leader>uT", "<cmd>TransparentToggle<CR>", desc = "Toggle Transparency" },
+    },
+  },
+}
