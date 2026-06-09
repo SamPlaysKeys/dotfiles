41cd9d10b37071fe63a6e41a27de1a387f69db56
SamPlaysKeys <sam@samplayskeys.com>
2026-06-09 11:31:03 -0400

    Try out a new telescope config

--- configs/nvim/lua/core/keymaps.lua
+++ configs/nvim/lua/core/keymaps.lua
@@ -85,3 +85,10 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 -- quit without saving
 Map("n", "LH", "<cmd>q!<CR>", { desc = "Quit without saving" })
+
+-- telescope commands
+keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
+keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
+keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
+keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
+
